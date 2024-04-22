c12d1d3a9b7f7ec704b70ad873a1a784bb89c703
xinerki <simasadves@gmail.com>
2024-04-22 12:15:49 +0200

    disable running when aiming teehee

--- client.lua
+++ client.lua
@@ -32,8 +32,14 @@ weapons_disabled = {
 
 -- the code
 
+function lerp(x1, x2, t)
+	return x1 + (x2 - x1) * t
+end
+
 last_sync = 0
 sync_interval = 100 -- sync every nth ms
+
+local recoil = vec(0.0, 0.0, 0.0)
 
 CreateThread(function()
 	while true do Wait(0)
@@ -155,6 +161,42 @@ CreateThread(function()
 					false, false, 0, false
 				)
 			end
+		else
+			if IsAimCamActive() then
+				DisableControlAction(0, 21, true)
+			end
+
+			/*
+			-- local pos = GetPedBoneCoords(PlayerPedId(), 57005, 0.0, 0.0, 0.0)
+
+			local x = math.sin(math.rad(-GetEntityHeading(PlayerPedId())))
+			local y = math.cos(math.rad(-GetEntityHeading(PlayerPedId())))
+
+			local pos = GetPedBoneCoords(PlayerPedId(), 18905, 0.0, 0.0, 0.0) + vec(x, y, 0.2)
+
+			local rayEnd = pos + vec(0.0, 0.0, -0.25)
+			local ray = StartExpensiveSynchronousShapeTestLosProbe(pos.x, pos.y, pos.z, rayEnd.x, rayEnd.y, rayEnd.z, 1 | 2 | 16, -1, 0)
+			local ret1, hit, _end, ret2, hitEnt = GetShapeTestResult(ray)
+
+			-- DrawLine(pos.x, pos.y, pos.z, rayEnd.x, rayEnd.y, rayEnd.z, 255, 255, 255, 255)
+
+			if hit ~= 0 and IsAimCamActive() then
+				local rpos = GetPedBoneCoords(PlayerPedId(), 57005, 0.0, 0.0, 0.0)
+				SetIkTarget(PlayerPedId(), 4, nil, nil, rpos.x, rpos.y, _end.z + 0.1, 0.0, 200, 200)
+			end
+			*/
+
+			-- if IsPedShooting(PlayerPedId()) then
+			-- 	-- recoil += vec(0.0, 0.0, 0.25)
+			-- 	local x = math.sin(math.rad(-GetEntityHeading(PlayerPedId()) + 180.0))
+			-- 	local y = math.cos(math.rad(-GetEntityHeading(PlayerPedId()) + 180.0))
+
+			-- 	recoil += vec(x, y, 0.0) * 0.25
+			-- end
+
+			-- recoil = lerp(recoil, vec(0.0, 0.0, 0.0), GetFrameTime() * 10.0)
+
+			-- SetIkTarget(PlayerPedId(), 4, nil, nil, pos.x + recoil.x, pos.y + recoil.y, pos.z + recoil.z, 0.0, 200, 200)
 		end
 		
 		if GetGameTimer() > last_sync + sync_interval then
